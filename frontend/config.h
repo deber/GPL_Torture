6d478a70780a39b1632920d9f4a164824bbf2949
deber <denis.bernard@laposte.net>
2017-08-11 15:48:28 +0200

    Added autotools

---
+++ frontend/config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "gpltorture"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "deber@users.sf.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "gpltorture"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "gpltorture 9999(r1)"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "gpltorture"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "9999(r1)"
+
+/* Version number of package */
+#define VERSION "9999(r1)"
